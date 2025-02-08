be4dd2cf7951076c705498e087ceb49ac53103b8
Dimitrios Pliakos <dimitrispl96@gmail.com>
2025-02-08 07:24:56 +0200

    04-backends

---
+++ 04-backends/providers.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "tf-test-s3-backend-01"
+    key    = "04-backends/state.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+
+}
