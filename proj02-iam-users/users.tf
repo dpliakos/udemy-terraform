9ba3d13a07eb1425d79b2cfe58616d3dee86a976
Dimitrios Pliakos <dimitrispl96@gmail.com>
2025-02-09 20:16:55 +0200

    proj02-iam-users

---
+++ proj02-iam-users/users.tf
@@ -0,0 +1,36 @@
+locals {
+  users_from_yaml = yamldecode(file("${path.module}/user-roles.yaml")).users
+  users_map = {
+    for config in local.users_from_yaml : config.username => config.roles
+  }
+}
+
+resource "aws_iam_user" "users" {
+  for_each = toset(local.users_from_yaml[*].username)
+  name     = each.value
+}
+
+resource "aws_iam_user_login_profile" "users" {
+  for_each        = aws_iam_user.users
+  user            = each.value.name
+  password_length = 8
+
+  lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key
+    ]
+  }
+}
+
+output "passwords" {
+  value = {
+    for user, user_login in aws_iam_user_login_profile.users : user => user_login.password
+  }
+  sensitive = true
+}
+
+output "users" {
+  value = local.users_from_yaml
+}
